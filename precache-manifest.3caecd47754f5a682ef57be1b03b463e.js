ee0c626a41e234b45814000aec902fbfb9020e16
Junior Dallagnolo <ernilodjr@gmail.com>
2018-11-03 23:54:47 -0400

    Updates

---
+++ precache-manifest.3caecd47754f5a682ef57be1b03b463e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstreactproject/static/media/loader.5d5d9eef.svg"
+  },
+  {
+    "revision": "491233f30e9506409c62",
+    "url": "/firstreactproject/static/js/runtime~main.491233f3.js"
+  },
+  {
+    "revision": "14c1f69749492aef025b",
+    "url": "/firstreactproject/static/js/main.14c1f697.chunk.js"
+  },
+  {
+    "revision": "50227dfa094e8faedba2",
+    "url": "/firstreactproject/static/js/1.50227dfa.chunk.js"
+  },
+  {
+    "revision": "14c1f69749492aef025b",
+    "url": "/firstreactproject/static/css/main.69b25b50.chunk.css"
+  },
+  {
+    "revision": "50227dfa094e8faedba2",
+    "url": "/firstreactproject/static/css/1.e398694e.chunk.css"
+  },
+  {
+    "revision": "879c59f65ede3a7ba0f12bfcc3ab06eb",
+    "url": "/firstreactproject/index.html"
+  }
+];
